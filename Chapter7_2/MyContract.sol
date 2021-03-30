6a63acfa605fa5028f25244f82b0fc935e208bd9
mychitt <mychitt78@gmail.com>
2021-03-30 08:08:49 +0700

    chi-commit

---
+++ Chapter7_2/MyContract.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.11;
+
+contract MyContract {
+    string public MyString = "Hello the real world..hihi ????";
+}
---
+++ Chapter7_7/SendMoney.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.13;
+
+contract SendMoney{
+    
+    uint public balanceReceived;
+    
+    function receiveMoney() public payable{
+        balanceReceived += msg.value;
+    }
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function withDrawMoney() public {
+        address payable to = msg.sender;
+        
+        to.transfer(this.getBalance());
+    }
+    
+    function withDrawMoneyTo(address payable _to) public{
+        _to.transfer(this.getBalance());
+    }
+}
